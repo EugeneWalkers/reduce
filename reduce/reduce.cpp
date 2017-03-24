63aeb0289359edb26c60098920f7893405862814
EugeneWalkers <evgeniy.skorohodov@gmail.com>
2017-03-25 02:59:26 +0300

    files uploaded

---
+++ reduce/reduce.cpp
@@ -0,0 +1,30 @@
+// reduce.cpp : Defines the entry point for the console application.
+//
+
+#include <string>
+#include <iostream>
+
+int main()
+{
+	bool wasText = false;
+	std::string temp;
+	std::getline(std::cin, temp, '\t');
+	std::string t = temp;
+	std::string A;
+	std::getline(std::cin, A);
+	int res = stoi(A);
+	while (std::getline(std::cin, temp, '\t') && std::getline(std::cin, A)){
+		if (t == temp) {
+			res += stoi(A);
+			wasText = true;
+		}
+		else {
+			std::cout << t << "\t" << res << std::endl;
+			t = temp;
+			res = stoi(A);
+		}
+	}
+	std::cout << t << "\t" << res << std::endl;
+    return 0;
+}
+
